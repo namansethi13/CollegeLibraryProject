c2195b721cfec30e003c8ed7c29dc8c3c54464a1
cheshta1828 <cheshtaabatra1828@gmail.com>
2023-06-10 11:20:22 +0530

    issue

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',include('Books_App.urls')),
-    path('user/', include('user.urls'))
+
+    path('books/', include(('Books_App.urls', 'Books_App'), namespace='Books_App')),
+    path('user/', include('user.urls')),
+    #path('submit_enrollment/',views.submit_enrollment,name="submit_enrollment")
+
+    #path('/issuebook/',)
     
 ]
